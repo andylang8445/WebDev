a243a6ef9bb699e725acc359d106f81efccac377
Hongjun Yun <46326335+andylang8445@users.noreply.github.com>
2020-09-06 01:15:13 -0400

    Update socket.js

--- Sep6,2020/NodeJS/socket.js
+++ Sep6,2020/NodeJS/socket.js
@@ -2,7 +2,7 @@ var io = require('socket.io').listen(3303);
 
 console.log('socket server run!!');
 
-io.sockes.on('connection', function (socket) {
+io.sockets.on('connection', function (socket) {
     socket.on('call', function (data) {
         console.log(data);
         socket.overrideMimeType('answer', 'hi~~~!~!');
